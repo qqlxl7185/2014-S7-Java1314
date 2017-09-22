ca774947cee2b646151ba2e3f0c652a515a300b7
lxl2016 <lxlxl2016@outlook.com>
2017-09-22 09:56:07 +0800

    SSM spring+spring MVC

--- ssm01/src/main/java/com/demo/org/controller/IndexController.java
+++ ssm01/src/main/java/com/demo/org/controller/IndexController.java
@@ -1,14 +1,26 @@
 package com.demo.org.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.demo.org.bean.Account;
+import com.demo.org.service.IAccountService;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+	@Resource
+	private IAccountService accountService;
+	
 	
 	@RequestMapping()
-	public String index() {
+	public String index(ModelMap resultMap) {
+		Account acc = accountService.getAccount(1);
+		resultMap.addAttribute("acc",acc);
+		
 		return "index";
 	}
 }
